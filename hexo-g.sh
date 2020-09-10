1e743fb0d4a79e46de43996a7fdcecf4670940e1
zx <zuxuan8@aliyun.com>
2020-09-10 17:57:23 +0800

    file

--- hexo-g.sh
+++ hexo-g.sh
@@ -1,2 +1,4 @@
 #!/usr/bin/env sh
 hexo clean && hexo g && hexo d
+
+# 将新发布的内容提交的GitHub接管
---
+++ push.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+git add -f * && git commit  -m "file" && git push -u origin master
+
+# 将hexo目录中的数据上传到远程data仓库做备份。
