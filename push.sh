57ed069d731a68143cbe5d09a0eec6640684485c
zx <zuxuan8@aliyun.com>
2020-11-04 10:17:02 +0800

    file

--- push.sh
+++ push.sh
@@ -2,14 +2,17 @@
 git add .	&> /dev/null
 git commit  -m "file"	&> /dev/null
 
-expect -c "
-set timeout 1
+#expect -c "
+#set timeout 1
 spawn git push -u origin master
-expect \"Username:\"
-send \"zuxuan8@aliyun.com\r\"
-expect \"Password:\"
-send \"zuxuan520.\r\"
-interact"
+expect "Username for 'https://github.com':"
+#send \"zuxuan8@aliyun.com\r\"
+send "zuxuan8@aliyun.com\n"
+expect "Password for 'https://zuxuan8@aliyun.com@github.com':"
+#send \"zuxuan520.\r\"
+send "zuxuan520.\n"
+#interact"
+expect off
 
 
 # 将hexo目录中的数据上传到远程data仓库做备份。
